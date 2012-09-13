3dac6df36743e3d3edabf2a77b5c6583da40188d
Julio Budal <juli0w@hotmail.com>
2012-09-12 23:43:50 -0300

    fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,34 +29,18 @@ after "deploy", "deploy:cleanup"
 
 namespace :uploads do
 
-  desc <<-EOD
-    Creates the upload folders unless they exist
-    and sets the proper upload permissions.
-  EOD
-  task :setup, :except => { :no_release => true } do
-    dirs = uploads_dirs.map { |d| File.join(shared_path, d) }
-    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
-  end
-
-  desc <<-EOD
-    [internal] Creates the symlink to uploads shared folder
-    for the most recently deployed version.
-  EOD
-  task :symlink, :except => { :no_release => true } do
+  desc "copy the assets for the shared path"
+  task :copy, :except => { :no_release => true } do
+    run "cp -r #{release_path}/public/uploads #{shared_path}/public"
     run "rm -rf #{release_path}/public/uploads"
-    run "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
   end
 
-  desc <<-EOD
-    [internal] Computes uploads directory paths
-    and registers them in Capistrano environment.
-  EOD
-  task :register_dirs do
-    set :uploads_dirs,    %w(uploads)
-    set :shared_children, fetch(:shared_children) + fetch(:uploads_dirs)
+  desc "get the assets back"
+  task :get, :except => { :no_release => true } do
+    run "cp -r #{shared_path}/public/uploads #{current_path}/public"
   end
 
-  after       "deploy:finalize_update", "uploads:symlink"
-  on :start,  "uploads:register_dirs"
+  after       "deploy:finalize_update", "uploads:get"
+  on :start,  "uploads:copy"
 
 end
