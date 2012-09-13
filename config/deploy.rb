b41533071d4a1d9d13c582e6634863028af668a6
Julio Budal <juli0w@hotmail.com>
2012-09-12 23:21:31 -0300

    fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ namespace :assets  do
   namespace :symlinks do
     desc "Setup application symlinks for shared assets"
     task :setup, :roles => [:app, :web] do
-      shared_assets.each { |link| run "mv -r #{shared_path}/#{link}" }
+      shared_assets.each { |link| run "mv #{shared_path}/#{link}" }
     end
 
     desc "Link assets for current deploy to the shared location"
